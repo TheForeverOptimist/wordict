e6f15334b8cddccd04b324b08c9124b358f4f6ef
TheForeverOptimist <sean@elevate.app>
2024-04-10 09:43:04 -0700

    added dashboard and ui components, i.e. lamp and created useState for inputdisplay

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,23 @@
+"use client"
+import InputDisplay from '@/components/InputDisplay'
+import { LampDemo } from '@/components/ui/LampDemo'
+import React from 'react'
+import { useState } from "react"
+
 
 
 
 
 export default function Dashboard(){
+    const[inputValue, setInputvalue] = useState('')
+
+
+
     return(
-        <h1>Hello, Dashboard Page</h1>
+        <>
+        <LampDemo />
+        <InputDisplay />
+        
+        </>
     )
 }
