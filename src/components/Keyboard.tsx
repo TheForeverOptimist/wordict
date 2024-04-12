1ff02a351cbdde891e9de5899a9fb3190f5319d4
TheForeverOptimist <sean@elevate.app>
2024-04-11 19:11:53 -0700

    keyboard interface is mobile responsive with iPhone14 comp

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,40 +1,40 @@
 export default function Keyboard() {
   return (
     <div className="fixed inset-x-0 bottom-0 bg-gray-100 p-4">
-      <div className="flex justify-center gap-2 mb-2">
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="Q">
+      <div className="flex justify-center gap-1 mb-2">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="Q">
           Q
         </button>
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="W">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="W">
           W
         </button>
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="E">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="E">
           E
         </button>
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="R">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="R">
           R
         </button>
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="T">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="T">
           T
         </button>
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="Y">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="Y">
           Y
         </button>
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="U">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="U">
           U
         </button>
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="I">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="I">
           I
         </button>
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="O">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="O">
           O
         </button>
-        <button className="border-2 size-12 text-3xl bg-slate-400" data-key="P">
+        <button className="border-2 w-10 h-10 text-xl md:w-12 md:h-12 md:text-3xl bg-slate-400" data-key="P">
           P
         </button>
       </div>
       <div className="space-x-2"> </div>
-      <div className="flex justify-center gap-2 mb-2">
+      <div className="flex justify-center gap-1 mb-2">
         <button className="border-2 size-12 text-3xl bg-slate-400" data-key="A">
           A
         </button>
@@ -64,7 +64,7 @@ export default function Keyboard() {
         </button>
       </div>
       <div className="space-x-2"> </div>
-      <div className="flex justify-center gap-2 mb-2">
+      <div className="flex justify-center gap-1 mb-2">
         <button className="border-2 h-12 w-24 text-3xl bg-slate-400" data-enter>
           Enter
         </button>
