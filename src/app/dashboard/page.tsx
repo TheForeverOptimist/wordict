6dee53195f8feaf500bedc502a4b200f791a4e33
TheForeverOptimist <sean@elevate.app>
2024-04-16 15:39:02 -0700

    refactored guess grid ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,39 @@
 "use client"
-import GuessGrid from '@/components/GuessGrid'
+import GuessGrid from '../../components/GuessGrid'
 import InputDisplay from '@/components/InputDisplay'
 import Keyboard from '@/components/Keyboard'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
 
 
 export default function Dashboard(){
+    const [currentGuess, setCurrentGuess] = useState('');
+    const [guesses, setGuesses] = useState([]);
+    const correctWord = 'STOP'; // example
+
+
+    const handleInputChange = (event) => {
+        setCurrentGuess(event.target.value.toUpperCase());
+    }
+
+    const handleSubmitGuess =
 
 
 
-    return(
-        <div className='flex flex-col items-center justify-center'>
-            {new Array(10).fill(0).map((_, i) => (
-                <GuessGrid key={i} word={"test"} guess={"stop"} isGuessed={true} />
-            ))}
+
+    return (
+        <div className='flex flex-col items-center justify-center p-4'>
+            <input type="text" value={currentGuess} onChange={handleInputChange} className='mb-4 p-2 border border-gray-300' maxLength={4} />
+            <button onClick={handleSubmitGuess} className='mb-4 p-2 bg-blue-500 text-white'>
+                Play
+            </button>
+            {guesses.map((data, index) => {
+                <GuessGrid key={index} guess={data.guess} symbols={data.symbols} />
+            })}
             <Keyboard />
+
         </div>
     )
 }
--- src/components/GuessGrid.tsx
+++ src/components/GuessGrid.tsx
@@ -1,27 +1,13 @@
-import React, { ReactNode } from "react";
+import React from "react";
 
-export default function GuessGrid({ isGuessed, word, guess }) {
-  return (
-    <div>
-      <div className="grid grid-cols-4x4em grid-rows-10x4em mb-1 mt-6 gap-2">
-        {new Array(4).fill(0).map((_, i) => {
-          const bgColor = !isGuessed
-            ? "bg-black"
-            : guess[i] === word[i]
-            ? "bg-green-500"
-            : word.includes(guess[i])
-            ? "bg-yellow-500"
-            : "bg-black";
-          return (
-            <div
-              key={i}
-        className={`flex items-center justify-center w-16 h-16 border border-gray-400  text-white font-bold uppercase ${bgColor}`}
-            >
-              {guess[i]}
-            </div>
-          );
-        })}
+
+const GuessGrid = ({guess, symbols}) => {
+    return (
+      <div className="flex items-center justify-center space-x-2">
+        <div className="text-lg font-bold uppercase">{guess}</div>
+        <div className="text-lg">{symbols}</div>
       </div>
-    </div>
-  );
+    );
 }
+
+export default GuessGrid
