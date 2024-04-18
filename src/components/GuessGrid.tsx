662e39a8177ea5bc37898b4a2a8d87de5de87bb9
TheForeverOptimist <sean@elevate.app>
2024-04-18 12:44:01 -0700

    added blocks UI to each guess plus added some animation styles to global css

--- src/components/GuessGrid.tsx
+++ src/components/GuessGrid.tsx
@@ -4,8 +4,10 @@ import React from "react";
 const GuessGrid = ({ guess, symbols }) => {
   return (
     <div className="flex items-center justify-center space-x-2">
-      <div className="text-lg font-bold uppercase">{guess}</div>
-      <div className="text-lg">{symbols}</div>
+      {guess.split('').map((letter, index) => (
+        <div key={index} className="w-8 h-8 md:w-12 flex items-center justify-center border-2 text-xl font-bold uppercase">{letter}</div>
+      ))}
+      <div className="text-lg border border-black-300">{symbols}</div>
     </div>
   );
 };
