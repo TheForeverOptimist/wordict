b8ed043e277f51890c3372433ffb2af32ffe6610
TheForeverOptimist <sean@elevate.app>
2024-04-30 19:39:08 -0700

    working on components for gameplay

--- src/components/GuessRow.tsx
+++ src/components/GuessRow.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import LetterBox from './LetterBox';
+import SymbolSpot from './SymbolSpot';
 
-const GuessRow = ({letters = ['', '', '', ''], symbols = ['', '', '', '']}) => {
-    return(
-        <div className='flex items-center justify-start space-x-2 border w-[16em] h-10 p-2 m-2 bg-gray-200'>
-            {letters.map((letter, index) => (
-                <LetterBox key={index} letter={letter} />
-            ))}
-            {symbols.map((symbol, index) => (
-                <SymbolSpot key={index} symbol={symbol} />
-            ))}
+
+const GuessRow = ({guessNumber = 1, letters = ['', '', '', ''], onLetterInput, symbols = ['', '', '', '']}) => {
+    return (
+      <div className="flex items-center justify-start space-x-2 border w-[18em] h-14 p-2 m-2 bg-white rounded-md">
+        <div className="md:text-xl lg:text-2xl text-black font-bold">
+          {guessNumber}
         </div>
+        {Array.from({ length: 4 }).map((_, index) => (
+          <div
+            key={index}
+            className="w-6 h-10 bg-gray-300 items-center justify-center rounded-md"
+          >
+            {letters[index] || ""}
+          </div>
+        ))}
+        {symbols.map((symbol, index) => (
+          <SymbolSpot key={index} symbol={symbol} />
+        ))}
+      </div>
     );
 };
 
