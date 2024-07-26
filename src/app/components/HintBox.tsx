6cd7982f3a028f654a074ac3b9a5544380c8a6bd
TheForeverOptimist <sean@elevate.app>
2024-07-26 13:29:33 -0700

    fixed hintbox display error

--- src/app/components/HintBox.tsx
+++ src/app/components/HintBox.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 
-interface HintBoxProps{
-    hint: string;
+interface HintBoxProps {
+  hint: string;
 }
 
-const HintBox: React.FC<HintBoxProps>=({hint})=> {
-    return (
-        <div className='w-12 h-12 border-2 border-gray-300 flex items-center justify-center text-xl'>
-            {hint}
-        </div>
-    )
-}
+const HintBox: React.FC<HintBoxProps> = ({ hint }) => {
+  return (
+    <div className="w-8 h-8 flex items-center justify-center text-xl mx-1">
+      {hint === "✅" ? "✅" : hint === "✨" ? "✨" : ""}
+    </div>
+  );
+};
 
-
-export default HintBox
+export default HintBox;
